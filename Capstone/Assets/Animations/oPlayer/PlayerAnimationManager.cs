fb62ffe83ca1b93d99746bee984f46351d90dd1d
yuya2358 <samlee2003@naver.com>
2025-09-27 16:05:41 -0400

    Almost Finished. Must Check Landing Condition

--- Capstone/Assets/Animations/oPlayer/PlayerAnimationManager.cs
+++ Capstone/Assets/Animations/oPlayer/PlayerAnimationManager.cs
@@ -1,17 +1,26 @@
+using SkateGame;
 using UnityEngine;
 
 public class PlayerAnimationManager : MonoBehaviour
 {
     public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    InputController playerController;
+
     void Start()
     {
-        
+        playerController = Object.FindFirstObjectByType<InputController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerController.IsGrounded())
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0 ? true : Input.GetAxisRaw("Horizontal") > 0 ? false : spriteRenderer.flipX;
+        }
+
     }
 
     public void OnLandingAnimationFinished()
