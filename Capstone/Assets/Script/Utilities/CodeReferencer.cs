b13798cda6fefa622a4d8579e3b8840604e38e91
Neilander <yw6472@nyu.edu>
2025-10-06 19:57:41 -0400

    Revert "Merge branch 'Neil_testPlace'"
    This reverts commit ce772a505bbf599ae92fef156ffe735847c91a03, reversing
    changes made to 1c8ea0bb2b90d7a5334ffd53b2d99bb901e5b3c3.

--- Capstone/Assets/Script/Utilities/CodeReferencer.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class CodeReferencer : MonoBehaviour
-{
-    public MonoBehaviour reference;
-
-
-}
--- Capstone/Assets/Script/askcy/Track.cs
+++ Capstone/Assets/Script/askcy/Track.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public class Track : MonoBehaviour
 {
-    LogicalRail rail;
-
-    void Start()
-    {
-        rail = GetComponentInChildren<LogicalRail>();
-    }
-
     public Vector3 GetTrackPosition()
     {
         return transform.position;
@@ -17,11 +10,5 @@ public class Track : MonoBehaviour
     public Vector3 GetTrackDirection()
     {
         return transform.right;
-    }
-
-    public TrackDirComputeTool GetDirTool()
-    {
-        return new TrackDirComputeTool(rail);
-
     }
 }
