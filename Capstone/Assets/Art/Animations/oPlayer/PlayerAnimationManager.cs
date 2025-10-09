04aae3ba959cc8af27ba082c8be732fe436b483b
BilboLam <bilbo_bagginss_@outlook.com>
2025-10-09 18:06:53 -0400

    landing bug fixed

--- Capstone/Assets/Art/Animations/oPlayer/PlayerAnimationManager.cs
+++ Capstone/Assets/Art/Animations/oPlayer/PlayerAnimationManager.cs
@@ -1,22 +1,25 @@
 using SkateGame;
 using UnityEngine;
+using QFramework;
 
-public class PlayerAnimationManager : MonoBehaviour
+
+public class PlayerAnimationManager : MonoBehaviour, ICanGetModel, IBelongToArchitecture
 {
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    public IPlayerModel playerModel;
 
-    PlayerController playerController;
+    public IArchitecture GetArchitecture() => GameApp.Interface;
 
     void Start()
     {
-        playerController = Object.FindFirstObjectByType<PlayerController>();
+        playerModel = this.GetModel<IPlayerModel>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerController.IsGrounded())
+        if (playerModel.IsGrounded.Value)
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0 ? true : Input.GetAxisRaw("Horizontal") > 0 ? false : spriteRenderer.flipX;
         }
